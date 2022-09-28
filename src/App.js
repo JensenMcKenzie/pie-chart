f884ab1a82f6abc66b1cedee68965f66a6a4bdcd
jensenmckenzie <jensen.mckenzie@outlook.com>
2022-09-28 11:49:43 -0700

    test pie chart

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { Pie } from "@visx/shape";
+import { Group } from "@visx/group";
+import { Text } from "@visx/text";
+
+const dataList = [
+  { name: "First", amount: 70, color: "#0033ad"},
+  { name: "Second", amount: 30, color: "#00ffbd"},
+  { name: "Third", amount: 10, color: "#F7931A"},
+];
+
+function clicked(){
+  console.log('x');
+}
 
 function App() {
+  const [active, setActive] = useState(null);
+  const width = 500;
+  const half = width/2;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <svg width={width} height={width}>
+        <Group top={half} left={half}>
+          <Pie
+            data={dataList}
+            pieValue={data => data.amount}
+            outerRadius={half}
+            innerRadius={({data}) => {
+              const size = active && active.name == data.name ? 20:12;
+              return half - size;
+            }}
+            padAngle={0.01}
+          >
+            {(pie) => {
+              return pie.arcs.map((arc) => {
+                return (
+                  <g
+                    key={arc.data.symbol}
+                    onMouseEnter={() => setActive(arc.data)}
+                    onMouseLeave={() => setActive(null)}
+                  >
+                    <path d={pie.path(arc)} fill={arc.data.color}></path>
+                  </g>
+                );
+              });
+            }}
+          </Pie>
+
+          {active ? (
+            <>
+              <Text textAnchor="middle" fill="#000" fontSize={40} dy={-20}>
+                {`${active.name}`}
+              </Text>
+
+              <Text
+                textAnchor="middle"
+                fill={active.color}
+                fontSize={20}
+                dy={20}
+              >
+                {`${active.amount}`}
+              </Text>
+            </>
+          ) : (
+            <>
+              <Text textAnchor="middle" fill="#000" fontSize={20} dy={20} onClick={clicked}>
+                {`${dataList.length} Teams`}
+              </Text>
+            </>
+          )}  
+        </Group>
+      </svg>
+    </main>
   );
 }
 
